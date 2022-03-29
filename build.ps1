feb288e181930c8801f18f1537099f911c938c4e
EvolutionTag <99890357+EvolutionTag@users.noreply.github.com>
2022-03-29 03:04:31 +0300

    Updated building script

--- build.ps1
+++ build.ps1
@@ -1,14 +1,19 @@
+$jashelper = "D:\\Program Files\\WC3Edit\\jasshelper\\clijasshelper.exe"
+$wc3lni = "d:\\program files\\wc3edit\\wc3lni\\w2l.exe"
+$TempFolder = ".\\Temp" #warning! will be deleted after compilation
+
+
 $d = [System.IO.File]::Exists(".w3x");
  if ($d)
  {
 
      $location = Get-Location
-     mkdir ".\Temp"
-     Set-Location ".\Temp"
-    &"D:\Program Files\WC3Edit\jasshelper\clijasshelper.exe" ("--scriptonly","..\Scripts\\common.j", "..\Scripts\\blizzard.j","..\Precompiled\\Scripts\\war3map.j","..\Map\\war3map.j")
+     mkdir $TempFolder
+     Set-Location $TempFolder
+    &$jashelper ("--scriptonly","..\\Scripts\\common.j", "..\Scripts\\blizzard.j","..\Precompiled\\Scripts\\war3map.j","..\Map\\war3map.j")
     Set-Location $location
-    Remove-Item -Path:".\Temp" -Confirm:$false -force -recurse
-    &"d:\program files\wc3edit\wc3lni\w2l.exe" slk;
+    Remove-Item -Path:$TempFolder -Confirm:$false -force -recurse
+    &$wc3lni slk;
  }
  else 
  {
