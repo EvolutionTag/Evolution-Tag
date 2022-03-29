80294db4fe94877b5847355d535e35c49d1be5f4
EvolutionTag <99890357+EvolutionTag@users.noreply.github.com>
2022-03-29 03:10:45 +0300

    Updated building script

--- build.ps1
+++ build.ps1
@@ -1,6 +1,10 @@
 $jashelper = "D:\\Program Files\\WC3Edit\\jasshelper\\clijasshelper.exe"
 $wc3lni = "d:\\program files\\wc3edit\\wc3lni\\w2l.exe"
 $TempFolder = ".\\Temp" #warning! will be deleted after compilation
+$commonj = "..\\Scripts\\common.j"
+$blizzardj = "..\\Scripts\\blizzard.j"
+$war3MapToCompile = "..\\ToCompile\\Scripts\\war3map.j"
+$CompiledScriptPath = "..\\Map\\war3map.j"
 
 
 $d = [System.IO.File]::Exists(".w3x");
@@ -10,7 +14,7 @@ $d = [System.IO.File]::Exists(".w3x");
      $location = Get-Location
      mkdir $TempFolder
      Set-Location $TempFolder
-    &$jashelper ("--scriptonly","..\\Scripts\\common.j", "..\Scripts\\blizzard.j","..\Precompiled\\Scripts\\war3map.j","..\Map\\war3map.j")
+    &$jashelper ("--scriptonly",$commonj , $blizzardj,$war3MapToCompile,$CompiledScriptPath)
     Set-Location $location
     Remove-Item -Path:$TempFolder -Confirm:$false -force -recurse
     &$wc3lni slk;
