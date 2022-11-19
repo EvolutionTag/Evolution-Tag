2bdf41ae96304eb487d25c458e60702eb74a3a87
EvolutionTag <99890357+EvolutionTag@users.noreply.github.com>
2022-11-20 01:29:41 +0300

    fixed bug with building method

--- test.ps1
+++ test.ps1
@@ -2,10 +2,9 @@ $location = Get-Location
 . .\build\lib\paths.ps1
 
 $wc3 = ($wc3folder+"war3.exe")
-$mapfolder = (Get-Item $location).Parent.FullName
-$map = '"'+$mapfolder.ToString() + "\" + $name + ".w3x"+'"'
 
-$arg = @("-loadfile",$map,"-window")
+
+$arg = @("-loadfile",$name,"-window")
 Write-Output $arg
 
 Start-Process -FilePath $wc3 -ArgumentList $arg -WorkingDirectory $wc3folder
