721dc844f7eba5ec75752f3d620b71b235d5a1dc
EvolutionTag <99890357+EvolutionTag@users.noreply.github.com>
2022-12-30 00:38:23 +0300

    Updated webhook url for desync messages

--- scripts/DataDump.lua
+++ scripts/DataDump.lua
@@ -354,7 +354,7 @@ function DumpSimpleLogged()
 	local name = GetPlayerName(GetLocalPlayer())
 
 	local message = string.format("(Evolution Tag) Warning! Desync Happened, Player: %d/%d: %s",GetPlayerId(GetLocalPlayer()),plcnt,name)
-	local url = "https://discord.com/api/webhooks/1048384373650632845/rGc2yntdQqMz_plYfL82dbmcpPFezJbCfSrdKGVz0eAMwh9hqtOznxpX3FrJ3H78kPv0"
+	local url = "https://discord.com/api/webhooks/1058136672983863427/p2hUp07xnkadjkoEsopD7mCYLkmDYHr6iP2-hyqdrJDqeGNWE3udZpz3x1SNdjkE_C-t"
 	local payload_json = string.format('{"content":"%s"}',message)
 	
 	local freplay = GetTempPath().."/LastReplay.w3g"
