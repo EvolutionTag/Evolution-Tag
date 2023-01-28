bc16589b20c0ead07afd5357507aceeb29a19029
EvolutionTag <99890357+EvolutionTag@users.noreply.github.com>
2023-01-28 16:56:00 +0300

    disable debug printing

--- scripts/lua/settings.lua
+++ scripts/lua/settings.lua
@@ -10,53 +10,53 @@ local settings_path = "GoodTool/settings/settings.lua"
 Settings.path = settings_path
 
 local function rawset(key,mode)
-    print("rawset",key,mode)
+    --print("rawset",key,mode)
     if(mode==Settings.get(key)) then
-        print("key: ",key," == ",mode)
+        --print("key: ",key," == ",mode)
         return
     end
     local r,react = pcall(function() return Settings.reacts[key] end)
-    print("rawset after getting react")
+    --print("rawset after getting react")
     if(react) then
-        print("react finded",react)
+        --print("react finded",react)
         local r,s = pcall(react,mode);
         if(not r) then
-            print("error",s)
+            --print("error",s)
             Log(string.format("editing setting: %s, error: \n==========\n%s\n=================\n",s))
         else
-            print("unk result")
+            --print("unk result")
         end
     end
     Settings.modes[key] = mode
 end
 
 local function Set(key,mode)
-    print(string.format("calling set for %s",key))
+    --print(string.format("calling set for %s",key))
     rawset(key,mode)
-    print("saving settings")
+    --print("saving settings")
     Settings.save()
 end
 
 local function load() 
     local loaded = persistence.load(settings_path)
-    print("loading data")
+    --print("loading data")
     for key,mode in pairs(loaded) do
 
-        print(string.format("loadding value: "))
+        --print(string.format("loadding value: "))
         rawset(key,mode)
     end
 
 end
 
 local function Save()
-    print("saving settings")
+    --print("saving settings")
     os.execute(string.format("mkdir %q","GoodTool"))
     os.execute(string.format("mkdir %q","GoodTool\\settings\\"))
     persistence.store(settings_path,Settings.modes)
 end
 
 local function AddReact(key,callback)
-    print("added react: ",key," ",callback)
+    --print("added react: ",key," ",callback)
     Settings.reacts[key]=callback
 end
 
