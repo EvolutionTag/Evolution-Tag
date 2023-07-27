91bc01e4e4ee2a3ec21d90e4b449efaf5234701e
goodlyhero <goodlyhero@noreply.com>
2023-07-27 16:18:05 +0300

    updated settings

---
+++ build/settings_example.py
@@ -0,0 +1,21 @@
+settings = {
+"jasshelper":"./jasshelper.exe",
+"w3xlni":"./w3xlni/w2l.exe",
+"pjass":"./pjass.exe",
+
+
+"system":"linux",
+
+"script_folder":"./src/scripts",
+"script_output":"./",
+"script_main":"./war3map.j",
+
+"in_folder":"./",
+"out_file":"../ET36.07.w3x",
+
+
+
+"common":"scripts/common.j",
+"blizzard":"scripts/blizzard.j",
+"war3map":"map/war3map.j"
+}
